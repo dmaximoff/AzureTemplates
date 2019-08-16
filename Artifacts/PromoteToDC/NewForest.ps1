3fa78675b56c3de307ce6bfbf129c25773435c31
dmaximoff <38011962+dmaximoff@users.noreply.github.com>
2019-08-16 11:35:40 -0700

    Add files via upload

--- Artifacts/PromoteToDC/NewForest.ps1
+++ Artifacts/PromoteToDC/NewForest.ps1
@@ -1,2 +1,6 @@
+# Install AD DS tools
+Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools 
+# Enable network discovery
+netsh advfirewall firewall set rule group=”network discovery” new enable=yes
 # Promote machine to DC
 Install-ADDSForest  -domainname domtest.local -SafeModeAdministratorPassword (convertto-securestring "Badger1!" -asplaintext -Force) -Force
