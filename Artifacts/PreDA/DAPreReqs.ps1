faaf755506596c81a9e998adc6ff2f230956366a
dmaximoff <38011962+dmaximoff@users.noreply.github.com>
2019-08-19 15:36:31 -0700

    add line to mount azure file share

--- Artifacts/PreDA/DAPreReqs.ps1
+++ Artifacts/PreDA/DAPreReqs.ps1
@@ -9,3 +9,10 @@ NDP461-KB3102436-x86-x64-AllOS-ENU.exe /q /norestart
 Set-Service -Name Browser -StartupType Automatic
 Set-Service -Name Browser -Status Running
 
+# Mount Azure file share
+# Save the password so the drive will persist on reboot
+Invoke-Expression -Command "cmdkey /add:adalab7435.file.core.windows.net /user:Azure\adalab7435 /pass:cDbn182dElr9qMSs85n7nchyGEmbvGZw+6BVKsnfkIhZ+8SySm/gBt9rLMCfK7caquYSmPzjVIurXW04iTeTTw=="
+# Mount  dams-releases
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\adalab7435.file.core.windows.net\dams-releases"
+# Mount dams-builds
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\adalab7435.file.core.windows.net\dams-builds"
