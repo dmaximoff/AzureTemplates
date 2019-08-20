52891473a5f12c298838da3b7554050f3b8f017b
dmaximoff <38011962+dmaximoff@users.noreply.github.com>
2019-08-20 06:55:46 -0700

    Update NewForest.ps1

--- Artifacts/PromoteToDC/NewForest.ps1
+++ Artifacts/PromoteToDC/NewForest.ps1
@@ -1,9 +1,10 @@
+# Promote machine to DC
+Install-ADDSForest  -domainname domtest.local -SafeModeAdministratorPassword (convertto-securestring "Badger1!" -asplaintext -Force) -Force
+Start-Sleep -s 60
 # Install AD DS tools
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools 
 Start-Sleep -s 30
 # Enable network discovery
 netsh advfirewall firewall set rule group=”network discovery” new enable=yes
 Start-Sleep -s 30
-# Promote machine to DC
-# Install-ADDSForest  -domainname domtest.local -SafeModeAdministratorPassword (convertto-securestring "Badger1!" -asplaintext -Force) -Force
-Start-Sleep -s 30
+
