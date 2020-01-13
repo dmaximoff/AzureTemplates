f07f99bfe46a004f4befc9c9931befdf8698f5c7
dmaximoff <38011962+dmaximoff@users.noreply.github.com>
2020-01-13 14:18:00 -0800

    Update DA11Install.ps1

--- Artifacts/DA11Install/DA11Install.ps1
+++ Artifacts/DA11Install/DA11Install.ps1
@@ -1,3 +1,6 @@
+
+$File = "c:\packages\DAFileName.msi"
+
 # Disable UAC
 $osversion = (Get-CimInstance Win32_OperatingSystem).Version 
 $version = $osversion.split(".")[0] 
@@ -18,10 +21,9 @@ if ($version -eq 10) {
 }
  
  # Mount Azure share and copy DA to c:\packages
-Invoke-Expression -Command "cmdkey /add:adalab7435.file.core.windows.net /user:Azure\adalab7435 /pass:cDbn182dElr9qMSs85n7nchyGEmbvGZw+6BVKsnfkIhZ+8SySm/gBt9rLMCfK7caquYSmPzjVIurXW04iTeTTw=="
-New-PSDrive -Name E -PSProvider FileSystem -Root "\\adalab7435.file.core.windows.net\engineering"
-mkdir c:\da
-copy e:\repo\DesktopAuthority_11.0.0.463.exe C:\da
+# Invoke-Expression -Command "cmdkey /add:adalab7435.file.core.windows.net /user:Azure\adalab7435 /pass:cDbn182dElr9qMSs85n7nchyGEmbvGZw+6BVKsnfkIhZ+8SySm/gBt9rLMCfK7caquYSmPzjVIurXW04iTeTTw=="
+# New-PSDrive -Name E -PSProvider FileSystem -Root "\\adalab7435.file.core.windows.net\engineering"
+# copy e:\repo\DesktopAuthority_11.0.0.463.exe C:\packages
 
 # Add domain\sladmin to local Administrators
 $domain = (Get-WmiObject Win32_ComputerSystem).Domain
@@ -47,10 +49,10 @@ public class SFW {
 "@
 
 # Run DA installer as domain\admin
-$username = "$domain\dadmin"
-$password = "Badger1!"
+$username = "$domain\sladmin"
+$password = "Lionfish1!"
 $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
-Start-Process -FilePath "C:\da\DesktopAuthority_11.0.0.463.exe" -Credential ($credentials)
+Start-Process -FilePath $File -Credential ($credentials)
 Start-Sleep -s 30
 
 # Validate DA is running before proceding
